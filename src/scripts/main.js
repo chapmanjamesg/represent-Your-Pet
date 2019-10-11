36c90fea91fe3416f59154d281f86d81631e2644
James Chapman <chapmanjamesg@gmail.com>
2019-10-11 13:17:50 -0500

    finished pet sounds

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,5 +2,18 @@ const pet = {
     name: 'King Louie the Fourteenth',
     species: 'French Poodle',
     nickname: 'Louie',
-    age: 14
+    age: 14,
+    bark:  function ()  {
+        window.alert('WOOF!')
+    },
+    whine: function(){
+        window.alert('wwwhhhhimmmpppeeeerrr!')
+    },
+    pant: function(){
+        window.alert('pant pant pant')
+    }
 }
+
+pet.bark()
+pet.whine()
+pet.pant()
